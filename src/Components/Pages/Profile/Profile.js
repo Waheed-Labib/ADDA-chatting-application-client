cf086bc875d33c62612563f1a069f4b19b739dca
Waheed-Labib <labibwahid28@gmail.com>
2023-08-01 23:05:25 +0600

    profile page updated

--- src/Components/Pages/Profile/Profile.js
+++ src/Components/Pages/Profile/Profile.js
@@ -11,26 +11,38 @@ const Profile = () => {
             <img style={{ width: '300px' }} src={logo} alt=''></img>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <div className='profile'>
-                    <h2>Your Profile</h2>
-                    <div className='profile-img'>
-                        <img className='user-dp' src='https://i.ibb.co/qsSZ3B3/frog.jpg' alt=''></img>
-                        <h4>To Change the Image</h4>
-                        <Link>Add Image URL</Link>
-                        or, <Link>Choose An Avatar</Link>
-                    </div>
-                    <div className='profile-info'>
-                        <h3>Name:</h3>
-                        <p>Abul Hashem</p>
-                        <FaEdit></FaEdit>
-                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '50px' }}>
+                        <h2 style={{ marginBottom: '30px' }}>Your Profile</h2>
+                        <div className='profile-img'>
+                            <img className='user-dp' src='https://i.ibb.co/qsSZ3B3/frog.jpg' alt=''></img>
+                            <h4>To Change the Image</h4>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                                <Link>Add Image URL</Link>
+                                <p>or,</p>
+                                <Link>Choose An Avatar</Link>
+                            </div>
 
-                    <div className='profile-info'>
-                        <h3>Email:</h3>
-                        <p>abulhashem@gmail.com</p>
-                        <small style={{ color: 'maroon' }}>(immutable)</small>
+                        </div>
+                        <div className='profile-info'>
+                            <h3>Name:</h3>
+                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                <p style={{ marginRight: '5px' }}>Abul Hashem</p>
+                                <FaEdit className='edit-icon'></FaEdit>
+                            </div>
+
+                        </div>
+
+                        <div className='profile-info'>
+                            <h3>Email:</h3>
+                            <p>abulhashem@gmail.com</p>
+                        </div>
                     </div>
+                    <p className='special-note'>*Email is immutable</p>
+                    <p className='special-note'>*You have not verified your email yet. <Link>Verify Email</Link></p>
                 </div>
+
             </div>
+
         </div>
 
 
