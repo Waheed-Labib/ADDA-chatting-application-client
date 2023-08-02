e3ddcf6e00e5d98104ffd68f91d30a86631da9ae
Waheed-Labib <labibwahid28@gmail.com>
2023-08-02 19:23:42 +0600

    profile page updated

--- src/Components/Pages/Profile/Profile.js
+++ src/Components/Pages/Profile/Profile.js
@@ -5,28 +5,25 @@ import { FaEdit } from 'react-icons/fa';
 import logo from '../../../Assets/images/logo/Adda Logo.png'
 import { AuthContext } from '../../../contexts/AuthProvider';
 import frog from '../../../Assets/images/avatar/frog.webp'
-import { toast } from 'react-hot-toast';
 
 const Profile = () => {
 
-    const [showImageInput, setShowImageInput] = useState(false)
     const { user, updateUserAccount } = useContext(AuthContext)
-    const [image, setImage] = useState(user?.photoURL)
-    console.log(user);
+    const [editName, setEditName] = useState(false)
+    const [name, setName] = useState(user?.displayName)
+    console.log(user)
 
-    const handleImageURLInput = event => {
-
-        const photo = event.target.imageURL.value;
+    const handleUpdateName = event => {
+        event.preventDefault();
+        const newName = event.target.name.value;
         const profile = {
-            photoURL: photo
+            displayName: newName
         }
         updateUserAccount(profile)
             .then(() => {
-                setImage(photo)
-                setShowImageInput(false)
+                setName(newName)
+                setEditName(false)
             })
-            .catch(err => toast.error(err.message))
-
     }
 
     return (
@@ -37,47 +34,44 @@ const Profile = () => {
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '50px' }}>
                         <h2 style={{ marginBottom: '30px' }}>Your Profile</h2>
                         <div className='profile-img'>
-                            {
-                                user?.photoURL ?
-                                    <img className='user-dp' src={image} alt=''></img>
-                                    :
-                                    <img className='user-dp' src={frog} alt=''></img>
-                            }
+
+                            <img className='user-dp' src={frog} alt=''></img>
+
                             <h4>To Change the Image</h4>
                             <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                                <Link onClick={() => setShowImageInput(!showImageInput)}>Add Image URL</Link>
+                                <Link>Upload Image</Link>
                                 <p>or,</p>
                                 <Link>Choose An Avatar</Link>
                             </div>
                         </div>
-                        {
-                            showImageInput &&
-                            <div style={{ width: '100%' }}>
-                                <form onSubmit={handleImageURLInput} className='image-input'>
-                                    <input className='image-url-input' type='text' placeholder='Image URL' name='imageURL'></input>
-                                    <input className='submit-img' type='submit' value='Update DP'></input>
-                                    <button onClick={() => setShowImageInput(false)} className='cancel-img'>Cancel</button>
-                                </form>
-                                <p><small>*Please ensure that you provide a valid URL</small></p>
-                                <p><small>*One way of doing that is to <span style={{ color: 'blue' }}>right click</span> on your online image and <span style={{ color: 'blue' }}>select 'copy image link'</span></small></p>
-                            </div>
 
-                        }
-
+                        {/* name */}
                         <div className='profile-info'>
                             <h3>Name:</h3>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <p style={{ marginRight: '5px ' }}>Abul Hashem</p>
-                                <FaEdit className='edit-icon'></FaEdit>
-                            </div>
+                            {
+                                editName ?
+                                    <form className='name-form' onSubmit={handleUpdateName}>
+                                        <input className='name-input' type='text' defaultValue={user?.displayName} name='name'></input>
+                                        <input className='name-submit' type='submit' value='Update Name'></input>
+                                    </form>
+                                    :
+                                    <div onClick={() => setEditName(true)} style={{ display: 'flex', alignItems: 'center' }}>
+                                        <p style={{ marginRight: '5px ' }}>{name || user?.displayName}</p>
+                                        <FaEdit className='edit-icon'></FaEdit>
+                                    </div>
+                            }
+
 
                         </div>
 
+                        {/* email */}
                         <div className='profile-info'>
                             <h3>Email:</h3>
-                            <p>abulhashem@gmail.com</p>
+                            <p>{user?.email}</p>
                         </div>
                     </div>
+
+                    {/* special note */}
                     <p className='special-note'>*Email is immutable</p>
                     {
                         !user?.emailVerified &&
